449b45269dbe18614f95c1cd4388865c48d113ee
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-07-26 16:44:21 +0530

    Added basic portfolio

---
+++ src/assets/index.js
@@ -0,0 +1,76 @@
+// UI Logo
+import logo from "./ui/logo.png";
+import menu from "./ui/menu.svg";
+import close from "./ui/close.svg";
+
+// company Logo
+import wiseneosco from "./company/wise.png";
+
+// Developer Logo
+import backend from "./developer/backend.png";
+import creator from "./developer/creator.png";
+import mobile from "./developer/mobile.png";
+import web from "./developer/web.png";
+
+// Technology Logo
+import docker from "./tech/docker.svg";
+import figma from "./tech/figma.png";
+import git from "./tech/git.svg";
+import javascript from "./tech/javascript.svg";
+import mongodb from "./tech/mongodb.svg";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.svg";
+import typescript from "./tech/typescript.svg";
+import express from './tech/express.png'
+import aws from './tech/aws.png'
+import sql from './tech/sql.png'
+import java from './tech/java.svg';
+import springboot from './tech/spring.svg';
+
+
+
+// Project Images
+import carrent from "./project/carrent.png";
+import jobit from "./project/jobit.png";
+
+// Social Logo
+import linkedin from './social/linkedin.png';
+import github from "./social/github.png";
+import gmail from './social/gmail.png'
+import whatsapp from './social/whatsapp.png';
+
+export {
+  wiseneosco,
+  logo,
+  menu,
+  close,
+
+  backend,
+  creator,
+  mobile,
+  web,
+
+  docker,
+  figma,
+  git,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  tailwind,
+  typescript,
+  sql,
+  aws,
+  express,
+  java,
+  springboot,
+  
+  carrent,
+  jobit,
+
+  github,
+  gmail,
+  linkedin,
+  whatsapp,
+};
